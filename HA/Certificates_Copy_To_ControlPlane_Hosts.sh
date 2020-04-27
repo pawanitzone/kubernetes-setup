58b2b1c5cf6afcdd7b3b0758d1935e2469518dea
Pawan Kumar <pawanitzone.kumar@gmail.com>
2020-04-27 12:53:27 +0530

    Update Certificates_Copy_To_ControlPlane_Hosts.sh

--- HA/Certificates_Copy_To_ControlPlane_Hosts.sh
+++ HA/Certificates_Copy_To_ControlPlane_Hosts.sh
@@ -1,3 +1,7 @@
+##Change some file permission to copy root file with normal user
+##sudo chmod 644 /etc/kubernetes/admin.conf /etc/kubernetes/pki/ca.key /etc/kubernetes/pki/etcd/ca.key /etc/kubernetes/pki/front-proxy-ca.key /etc/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.pub
+## Revert changes:
+##sudo chmod 600 /etc/kubernetes/admin.conf /etc/kubernetes/pki/ca.key /etc/kubernetes/pki/etcd/ca.key /etc/kubernetes/pki/front-proxy-ca.key /etc/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.pub
 USER=root # customizable
 CONTROL_PLANE_IPS="192.168.56.136 192.168.56.137"
 for host in ${CONTROL_PLANE_IPS}; do
