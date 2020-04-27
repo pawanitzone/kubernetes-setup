2588c7b7681b1843375676e408eee251f0bbc609
Pawan Kumar <pawanitzone.kumar@gmail.com>
2020-04-27 12:54:57 +0530

    Update Certificates_Copy_To_ControlPlane_Hosts.sh

--- HA/Certificates_Copy_To_ControlPlane_Hosts.sh
+++ HA/Certificates_Copy_To_ControlPlane_Hosts.sh
@@ -2,16 +2,18 @@
 ##sudo chmod 644 /etc/kubernetes/admin.conf /etc/kubernetes/pki/ca.key /etc/kubernetes/pki/etcd/ca.key /etc/kubernetes/pki/front-proxy-ca.key /etc/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.pub
 ## Revert changes:
 ##sudo chmod 600 /etc/kubernetes/admin.conf /etc/kubernetes/pki/ca.key /etc/kubernetes/pki/etcd/ca.key /etc/kubernetes/pki/front-proxy-ca.key /etc/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.pub
-USER=root # customizable
-CONTROL_PLANE_IPS="192.168.56.136 192.168.56.137"
+USER=centos # customizable
+CONTROL_PLANE_IPS="172.16.1.163 172.16.1.217"
 for host in ${CONTROL_PLANE_IPS}; do
-    scp /etc/kubernetes/pki/ca.crt "${USER}"@$host:
-    scp /etc/kubernetes/pki/ca.key "${USER}"@$host:
-    scp /etc/kubernetes/pki/sa.key "${USER}"@$host:
-    scp /etc/kubernetes/pki/sa.pub "${USER}"@$host:
-    scp /etc/kubernetes/pki/front-proxy-ca.crt "${USER}"@$host:
-    scp /etc/kubernetes/pki/front-proxy-ca.key "${USER}"@$host:
-    scp /etc/kubernetes/pki/etcd/ca.crt "${USER}"@$host:etcd-ca.crt
-    scp /etc/kubernetes/pki/etcd/ca.key "${USER}"@$host:etcd-ca.key
-    scp /etc/kubernetes/admin.conf "${USER}"@$host:
+    mkdir /home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/ca.crt "${USER}"@$host:/home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/ca.key "${USER}"@$host:/home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/sa.key "${USER}"@$host:/home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/sa.pub "${USER}"@$host:/home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/front-proxy-ca.crt "${USER}"@$host:/home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/front-proxy-ca.key "${USER}"@$host:/home/"${USER}"/certs
+    scp -r /etc/kubernetes/pki/etcd/ca.crt "${USER}"@$host:/home/"${USER}"/certs/etcd-ca.crt
+    scp -r /etc/kubernetes/pki/etcd/ca.key "${USER}"@$host:/home/"${USER}"/certs/etcd-ca.key
+    scp -r /etc/kubernetes/admin.conf "${USER}"@$host:/home/"${USER}"/certs
 done
+
