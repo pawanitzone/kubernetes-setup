6c2a8aaefdb5661f54a3a473fd2f2c0f5b180ee9
pawanitzone <pawanitzone.kumar@gmail.com>
2020-04-27 08:58:52 +0530

    Added docs to setup k8s HA

---
+++ HA/Certificates_Copy_To_ControlPlane_Hosts.sh
@@ -0,0 +1,13 @@
+USER=root # customizable
+CONTROL_PLANE_IPS="192.168.56.136 192.168.56.137"
+for host in ${CONTROL_PLANE_IPS}; do
+    scp /etc/kubernetes/pki/ca.crt "${USER}"@$host:
+    scp /etc/kubernetes/pki/ca.key "${USER}"@$host:
+    scp /etc/kubernetes/pki/sa.key "${USER}"@$host:
+    scp /etc/kubernetes/pki/sa.pub "${USER}"@$host:
+    scp /etc/kubernetes/pki/front-proxy-ca.crt "${USER}"@$host:
+    scp /etc/kubernetes/pki/front-proxy-ca.key "${USER}"@$host:
+    scp /etc/kubernetes/pki/etcd/ca.crt "${USER}"@$host:etcd-ca.crt
+    scp /etc/kubernetes/pki/etcd/ca.key "${USER}"@$host:etcd-ca.key
+    scp /etc/kubernetes/admin.conf "${USER}"@$host:
+done
